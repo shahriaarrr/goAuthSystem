8c8844d4e9d478298e4f950b43449fa2013cd306
shahriaarrr <shahriaarrr@gmail.com>
2024-09-25 20:29:08 +0330

    add login controller

--- controllers/usersController.go
+++ controllers/usersController.go
@@ -4,8 +4,11 @@ import (
 	"go-jwt/initializers"
 	"go-jwt/models"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -53,4 +56,53 @@ func Signup(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "User created successfully",
 	})
+}
+
+func Login(c *gin.Context) {
+	var body struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	if err := c.ShouldBindJSON(&body); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to read body: " + err.Error(),
+		})
+		return
+	}
+
+	var user models.User
+	initializers.DB.First(&user, "email = ?", body.Email)
+	if user.ID == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid email or password",
+		})
+		return
+	}
+
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(body.Password))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid email or password",
+		})
+		return
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub": user.ID,
+		"exp": time.Now().Add(time.Hour * 24 * 30).Unix(),
+	})
+
+	secret := os.Getenv("SECRET")
+	tokenString, err := token.SignedString([]byte(secret))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to generate token",
+		})
+		return
+	}
+
+	c.SetSameSite(http.SameSiteLaxMode)
+	c.SetCookie("Authorization", tokenString, 3600*24*30, "", "", true, true)
+	c.JSON(http.StatusOK, gin.H{})
 }
--- main.go
+++ main.go
@@ -24,5 +24,7 @@ func main() {
 
 	r.POST("/signup", controllers.Signup)
 
+	r.POST("/login", controllers.Login)
+
 	r.Run()
 }
